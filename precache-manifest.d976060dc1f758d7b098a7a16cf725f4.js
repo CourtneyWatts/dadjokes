a0bcfc43f50ed3bc550a92877e25dda792ccfccf
CourtneyWatts <courtney.watts@icloudm>
2020-06-03 16:23:57 +0100

    Updates

---
+++ precache-manifest.d976060dc1f758d7b098a7a16cf725f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7c2fb3d250aedf4afaf8fa061c22a2",
+    "url": "/dadjokes/index.html"
+  },
+  {
+    "revision": "1cbbca1f7f63022912af",
+    "url": "/dadjokes/static/css/main.55c00ac5.chunk.css"
+  },
+  {
+    "revision": "c3f6c0719e092bdf6e0c",
+    "url": "/dadjokes/static/js/2.fc7d8cac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dadjokes/static/js/2.fc7d8cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cbbca1f7f63022912af",
+    "url": "/dadjokes/static/js/main.154a4f77.chunk.js"
+  },
+  {
+    "revision": "faa6961ae5c68046ab33",
+    "url": "/dadjokes/static/js/runtime-main.08b8f969.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dadjokes/static/media/logo.5d5d9eef.svg"
+  }
+]);
