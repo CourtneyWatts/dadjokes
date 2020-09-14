3defcdcfa3d3e83cf40710b1140ccfde01c2d340
CourtneyWatts <courtney.watts@icloud.com>
2020-09-14 13:14:53 +0100

    test git

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.scss'
-import JokeBoard from './JokeBoard'
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import JokeBoard from './JokeBoard';
 
 function App() {
   return (
     <div className="App">
-        <JokeBoard />
+      baba
+      <JokeBoard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
